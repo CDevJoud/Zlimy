43bb745f1ef98c4ead473179c2fd54fda03379c2
CDevJoud <97121061+CDevJoud@users.noreply.github.com>
2023-06-13 21:20:34 +0200

    Fixed Issue with InitFont Function

--- Zlimy/Engine/Font.hpp
+++ Zlimy/Engine/Font.hpp
@@ -1,4 +1,9 @@
 #pragma once
 #define FONTSIZE 32156
+#include <SFML/Graphics/Font.hpp>
+
+
+sf::Font font;
+void InitFont();
 
 unsigned char fontData[];
--- Zlimy/main.cpp
+++ Zlimy/main.cpp
@@ -1,8 +1,9 @@
-#include "Engine/Engine.h"
+#include "Engine/Engine.hpp"
 using namespace IExtreme::Engine;
 
 int main()
 {
+	InitFont();
 	Ugr::Engine e; 
 	e.run();
 	return 0;
